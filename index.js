b273912b347eaaf15963013f0d0d8ead848e2f86
Abhishek Kumar <abhishek.kasyap09@gmail.com>
2021-01-23 23:28:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const puppeteer = require('puppeteer-core');
+
+let scrape = async () => {
+ const browser = await puppeteer.launch({headless: false,executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'});
+ const page = await browser.newPage();
+ await page.setDefaultNavigationTimeout(0);
+ await page.goto('https://prefeitura.pbh.gov.br/saude/licitacao/pregao-eletronico-151-2020');
+ await page.waitForSelector('#block-gavias-vinor-page-title > div > h1 > span > font > font')
+ const heading1 = await page.$eval(
+    '#block-gavias-vinor-page-title > div > h1 > span > font > font',
+    el => el.textContent
+  )
+  console.log(heading1)
+ await page.waitForSelector('#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-nothing > span > span:nth-child(1) > font > font')
+ const publication = await page.$eval(
+    '#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-nothing > span > span:nth-child(1) > font > font',
+    el => el.textContent
+  )
+  console.log('PUBLICATION DATE: ' + publication)
+ await page.waitForSelector('#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-nothing > span > span:nth-child(19) > span > font > font')
+ const bidding = await page.$eval(
+    '#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-nothing > span > span:nth-child(19) > span > font > font',
+    el => el.textContent
+  )
+  console.log('BIDDING DATE: '+ bidding)
+ await page.waitForSelector('#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-nothing > span > span:nth-child(19) > font > font')
+ const object = await page.$eval(
+    '#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-nothing > span > span:nth-child(19) > font > font',
+    el => el.textContent
+  )
+  console.log('OBJECT: ' + object)
+ await page.waitForSelector('#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-field-historico-da-licitacao > div > table > tbody > tr > td:nth-child(2) > div > div > div > a > img')
+ const link = await page.$eval(
+    '#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-field-historico-da-licitacao > div > table > tbody > tr > td:nth-child(2) > div > div > div > a > img',
+    el => el.textContent
+  )
+  console.log('link: ' + link)
+await browser.close();
+};
+scrape().then((value) => {
+ // Success!
+});
